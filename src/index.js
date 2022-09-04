be219ccc8fec8da658de505ce8783a1fa1ed2560
unknown <jlrabdulazee@gmail.com>
2022-09-04 10:07:48 +0100

    added express 3

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
     <Provider store={store}>
     <App />
-    </Provider>
-  </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
